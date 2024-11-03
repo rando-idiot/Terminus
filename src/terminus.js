d3a0e41f8b0602fd48b7129727548efce297b54e
Rando <emmettmittenzwei@gmail.com>
2024-11-03 12:57:06 -0500

    fix because gh pages sucks

--- src/terminus.js
+++ src/terminus.js
@@ -745,7 +745,7 @@ terminal.addCommand(function loadmygame() {
 //Music engine, when adding song(s), place in `mus` folder as a number, then increment game.totalmus by 1. Eg, there are 5 songs, so if you want to add a 6th one, you place it in the mus folder as '6.wav' and set game.totalmus to 6.
 terminal.addCommand(function playasong() {
     let playedsong = (randomnumbah(1, game.totalmus)) 
-    let playedsongdir = "/resources/mus/" + playedsong + ".wav";
+    let playedsongdir = "/Terminus.js/resources/mus/" + playedsong + ".wav";
     let audio = new Audio(playedsongdir);
     audio.play();
 })
