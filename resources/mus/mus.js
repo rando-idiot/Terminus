ef189c083c44d62787b332321aac17e25c73894b
Rando <emmettmittenzwei@gmail.com>
2024-11-08 22:14:23 -0500

    same fix as prev but for mus engine

--- resources/mus/mus.js
+++ resources/mus/mus.js
@@ -1,5 +1,5 @@
 //Music engine, when adding song(s), place in `mus` folder as a number, then increment game.totalmus by 1. Eg, there are 5 songs, so if you want to add a 6th one, you place it in the mus folder as '6.wav' and set game.totalmus to 6.
-import { Terminal } from "../../src/lib/terminal";
+import { Terminal } from "../../src/lib/terminal.js";
 
 
 const totalmus = 1
