3cecd642160f17061f6ea65e3101bb7723c47738
Rando <emmettmittenzwei@gmail.com>
2024-11-08 22:27:14 -0500

    remerge music stuff into 'game'

--- src/terminus.js
+++ src/terminus.js
@@ -78,6 +78,7 @@ let game = events({
             game.points *= game.itemmult;
         }
     },
+    totalmus: 1,
 });
 
 /** @type {Terminal} */
@@ -633,9 +634,9 @@ terminal.addCommand(function loadmygame() {
 
 
 //Music engine, when adding song(s), place in `mus` folder as a number, then increment game.totalmus by 1. Eg, there are 5 songs, so if you want to add a 6th one, you place it in the mus folder as '6.wav' and set game.totalmus to 6.
-const totalmus = 1
+
 terminal.addCommand(function playasong() {
-    let playedsong = (randomnumbah(1, totalmus)) 
+    let playedsong = (randomnumbah(1, game.totalmus)) 
     let playedsongdir = "../resources/mus/" + playedsong + ".wav";
     let audio = new Audio(playedsongdir);
     audio.play();
