aba44253378f278e60c0db005ec31550f5573d87
Rando <emmettmittenzwei@gmail.com>
2024-11-08 22:15:03 -0500

    realize i dont need terminal for mus engine (i needed randomnumbah)

--- resources/mus/mus.js
+++ resources/mus/mus.js
@@ -1,6 +1,5 @@
 //Music engine, when adding song(s), place in `mus` folder as a number, then increment game.totalmus by 1. Eg, there are 5 songs, so if you want to add a 6th one, you place it in the mus folder as '6.wav' and set game.totalmus to 6.
-import { Terminal } from "../../src/lib/terminal.js";
-
+import { randomnumbah } from "../../src/lib/helpers.js";
 
 const totalmus = 1
 export function musengine() {
