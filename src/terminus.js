249334dc9a19af5429d54a65db5512b4d49ad5e2
Rando <emmettmittenzwei@gmail.com>
2024-11-08 22:22:41 -0500

    fixed shit

--- src/terminus.js
+++ src/terminus.js
@@ -632,4 +632,11 @@ terminal.addCommand(function loadmygame() {
 })
 
 
-terminal.addCommand(playasong())
+//Music engine, when adding song(s), place in `mus` folder as a number, then increment game.totalmus by 1. Eg, there are 5 songs, so if you want to add a 6th one, you place it in the mus folder as '6.wav' and set game.totalmus to 6.
+const totalmus = 1
+terminal.addCommand(function musengine() {
+    let playedsong = (randomnumbah(1, totalmus)) 
+    let playedsongdir = "../resources/mus/" + playedsong + ".wav";
+    let audio = new Audio(playedsongdir);
+    audio.play();
+})
