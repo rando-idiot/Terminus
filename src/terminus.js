0d2117ddcf1291d0e795321a39c3a1ce022e0728
Rando <emmettmittenzwei@gmail.com>
2024-11-21 22:22:30 -0500

    "fixed shit"

--- src/terminus.js
+++ src/terminus.js
@@ -2,7 +2,58 @@ import { Terminal } from "./lib/terminal.js";
 import { sleep, spawn, randomnumbah } from "./lib/helpers.js";
 import { Achievement } from "./lib/achievements.js";
 import { fish } from "./lib/fish.js";
+import { events } from "./lib/events.js";
 // import "./lib/skills.js"; // this is heavily broken rn, TODO: change this to use class (like achievements)
+
+let game = events({
+    terminal: new Terminal(document.body.querySelector("#terminal")),
+    gameverison: "0.2.6",
+    unlocks: events({
+        begin: false,
+        index: false,
+        doctype: false,
+        configyml: false,
+        infshop: false,
+    }),
+    misc: events({ // I do not know why i called this misc, just shove random shtuff here.
+        cantaffordskill: "You need more skill points.",
+        needparentskill: "Unlock the previous skill first.",
+    }),
+    skillpoints: 0,
+    xp: 0,
+    pointcalcstatus: false,
+    infstage: 0,
+    points: 0,
+    steponeadd: 0,
+    steptwomult: 1,
+    stepthreemult: 1,
+    stepfouradd: 1,
+    basegain: 1,
+    upgradebonus: 1,
+    upgpriceboost: 0,
+    upgstage: 0,
+    updateloop: 1,
+    power: 10,
+    powerpoints: 1, //Hahah PP
+    indebted: 1,
+    maxbattery: 15,
+    rechargerate: 1,
+    antipower: 10,
+    itemduration: 0,
+    pointcalc: () => {
+        game.pointcalcstatus = false;
+        game.points += (game.basegain +
+            game.steponeadd * game.steptwomult *
+                game.stepthreemult +
+            game.stepfouradd * game.powerpoints)
+             ;
+        if (game.itemduration > 0) {
+            game.itemduration -= 1;
+            game.points *= game.itemmult;
+        }
+    },
+    totalmus: 1,
+});
 
 // 0.2.6
 // MUSIC RELATED STUFF, SEE COMMIT MSGS
@@ -87,54 +138,7 @@ terminal.addCommand(function fullscreen() {
     console.log("Toggled fullscreen.")
   }
   );
-  let game = events({
-    gameverison: "0.2.6",
-    unlocks: events({
-        begin: false,
-        index: false,
-        doctype: false,
-        configyml: false,
-        infshop: false,
-    }),
-    misc: events({ // I do not know why i called this misc, just shove random shtuff here.
-        cantaffordskill: "You need more skill points.",
-        needparentskill: "Unlock the previous skill first.",
-    }),
-    skillpoints: 0,
-    xp: 0,
-    pointcalcstatus: false,
-    infstage: 0,
-    points: 0,
-    steponeadd: 0,
-    steptwomult: 1,
-    stepthreemult: 1,
-    stepfouradd: 1,
-    basegain: 1,
-    upgradebonus: 1,
-    upgpriceboost: 0,
-    upgstage: 0,
-    updateloop: 1,
-    power: 10,
-    powerpoints: 1, //Hahah PP
-    indebted: 1,
-    maxbattery: 15,
-    rechargerate: 1,
-    antipower: 10,
-    itemduration: 0,
-    pointcalc: () => {
-        game.pointcalcstatus = false;
-        game.points += (game.basegain +
-            game.steponeadd * game.steptwomult *
-                game.stepthreemult +
-            game.stepfouradd * game.powerpoints)
-             ;
-        if (game.itemduration > 0) {
-            game.itemduration -= 1;
-            game.points *= game.itemmult;
-        }
-    },
-    totalmus: 1,
-});
+
 
 
 const DEBUG_MODE = true;
