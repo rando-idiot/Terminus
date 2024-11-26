48f703b4a5033e0a00ec5d2c14a4ca9868c9e2e8
Bleb1k <viktor.masnikov17283940@gmail.com>
2024-11-26 11:50:50 +0900

    removed logstyles dependency from fish

--- src/lib/fish.js
+++ src/lib/fish.js
@@ -1,24 +1,20 @@
-import { randomnumber } from "./helpers.js"
-import { Terminal, logstyles } from "./terminal.js"
+import { randomnumber } from "./helpers.js";
+import { Terminal } from "./terminal.js";
 
 export class fish {
     constructor(name, desc, price, chance) {
-        this.name = name
-        this.desc = desc
-        this.price = price
-        this.chance = chance //Out of 100
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.chance = chance; //Out of 100
     }
-        catchafish() {
-            let didyacatchit = randomnumber(1, 100)
-            if (didyacatchit >= 0 && didyacatchit <= this.chance) {
-                terminal.log(logstyles.log, "You caught a" + this.name + "!");
-                terminal.log(logstyles.log, "''" + this.desc + "''");
-                game.points += this.price
-            }
+    catchafish() {
+        let didyacatchit = randomnumber(1, 100);
+        if (didyacatchit >= 0 && didyacatchit <= this.chance) {
+            // wont work as terminal is not defined in this scope, either provide it or localize this function to terminus.js
+            terminal.log("You caught a" + this.name + "!");
+            terminal.log("''" + this.desc + "''");
+            game.points += this.price;
         }
-    
-
+    }
 }
-
-
-
