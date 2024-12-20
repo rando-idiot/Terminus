5b234905a6d9e02781cc7a60ca92350e1c536d6a
Rando <emmettmittenzwei@gmail.com>
2024-12-19 22:15:23 -0500

    the base (tm) (im too lazy to finish this rn and i want to push it so you have a basic idea of what im doing)

--- src/lib/fish.js
+++ src/lib/fish.js
@@ -11,6 +11,7 @@ export class fish {
     catchafish() {
         let didyacatchit = randomnumber(1, 100)
         if (didyacatchit >= 0 && didyacatchit <= this.chance) {
+            import { Terminal } from '../terminal.js'
             // wont work as terminal is not defined in this scope, either provide it or localize this function to terminus.js
             terminal.log('You caught a' + this.name + '!')
             terminal.log("''" + this.desc + "''")
